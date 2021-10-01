04144654e3cc1d752bb8dd4f51ecb50b984d448f
camigarciapetiet <64105979+camigarciapetiet@users.noreply.github.com>
2021-10-01 18:20:26 -0300

    agregado de emotion Matching

---
+++ Proyecto Unity Diseño 2021/Assets/Scripts/UnityWithRasaDemoScene/Test.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityWithRasaDemoScene;
+
+namespace UnityWithRasaDemoScene
+{
+    public class Test : MonoBehaviour
+    {
+        public ReceiveMessageJson mensaje;
+        public EmotionMatching emotionMatching;
+        private void Start()
+        {
+            List<Tupla> conjGanador= emotionMatching.ObtenerConjGanador(mensaje);
+        }
+
+        [Serializable]
+        // ReceiveMessageJson extrae el valor del objeto json que se recibe del servidor rasa
+        // Se usa para extraer un solo mensaje devuelto por el bot
+        public class ReceiveMessageJson
+        {
+            public string recipient_id;
+            public Custom custom;
+            [System.Serializable]
+            public class Custom
+            {
+                public String text;
+                public String[] vector;
+            }
+        }
+    }
+}
