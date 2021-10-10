764796aefc3c1116a4db63a900b4213e32350bb6
Abachian <54768966+Abachian@users.noreply.github.com>
2021-10-10 01:41:31 -0300

    faltaAcomodarCejas

---
+++ Proyecto Unity Diseño 2021/Assets/Demo.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Demo : MonoBehaviour
+{
+    public GameObject p1;
+    public GameObject p2;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        p1.GetComponent<AnimationComposer>().AddBlock(new Block(new List<LayerInfo>()
+        {
+            new LayerInfo("StandingArguing"),
+            new LayerInfo("AngryFace")
+        }));
+        StartCoroutine(Esperar());        
+        
+    }
+
+    private IEnumerator Esperar()
+    {
+        yield return new WaitForSeconds(0f);
+        p2.GetComponent<AnimationComposer>().AddBlock(new Block(new List<LayerInfo>()
+        {
+            new LayerInfo("StandingArguing"),
+            new LayerInfo("AngryFace")
+        }));
+    }
+}
